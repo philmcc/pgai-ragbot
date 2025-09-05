6422f616efa7d5178885fe85f281d0550df05801
Philip McClarence <philipmcclarence@ip-192-168-6-20.ec2.internal>
2025-09-05 16:46:23 +0100

    Fix: grant matches app.search_chunks(text,int,float4)

--- db/init/06_grants.sql
+++ db/init/06_grants.sql
@@ -6,5 +6,5 @@ GRANT SELECT ON app.v_ingest_status TO anon;
 
 -- RPC functions
 GRANT EXECUTE ON FUNCTION app.chat_rag(text, int) TO anon;
-GRANT EXECUTE ON FUNCTION app.search_chunks(text, int) TO anon;
+GRANT EXECUTE ON FUNCTION app.search_chunks(text, int, float4) TO anon;
 GRANT EXECUTE ON FUNCTION app.run_ingest_once() TO anon;
